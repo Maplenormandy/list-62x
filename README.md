de026b5152a66b012879b9166612c3b0344d8b21
Norman Cao <maplenormandy@gmail.com>
2014-11-11 18:21:29 -0500

    Able to change exposure and feature detect

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ list-62x
 ========
 
 Light Invariant Stable Features auto camera setting correction algorithm, for MIT 16.62x Fall 2014 / Spring 2015
+
+Note on installation. Make sure you're using OpenCV 2.4.9
+
+    cd opencv
+    mkdir release
+    cd release
+    cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules ..
+    
+    make -j4
+    sudo make install
+
+Another helpful utility is Coriander, which is like cheese but for 1394 cameras
+
+    sudo apt-get install coriander
--- python/checkCameras.py
+++ python/checkCameras.py
@@ -1,16 +1,40 @@
 import cv2
+import math
 
 # This script is a quick test script to show the PointGrey Firefly image
 
 # Change 0 to the index that works
 cap = cv2.VideoCapture(0)
+cv2.namedWindow('frame')
+
+# Create a trackbar for setting the exposure manually
+def setCapExposure(x):
+    cap.set(15, x)
+cv2.createTrackbar('exposure', 'frame', 1, 531, setCapExposure)
+
+# fd feature detector. Note the high value is just for display
+fd = cv2.ORB()
+
+t = 0.0;
 
 if cap.isOpened():
     while True:
         ret, frame = cap.read()
-        # Convert the camera from bayer format to RGB
-        frame = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2BGR);
-        cv2.imshow('frame', frame)
+
+        if ret:
+            # Convert the camera from bayer format to RGB
+            frame = cv2.cvtColor(frame, cv2.COLOR_BAYER_BG2BGR);
+
+            # Use fd feature detector
+            kp = fd.detect(frame, None)
+            disp = cv2.drawKeypoints(frame, kp, None, (255,0,0), 4)
+
+            cv2.imshow('frame', disp)
+
+        # Try setting the camera settings
+
+
+        t += 0.030;
 
         if cv2.waitKey(30) & 0xFF == ord('q'):
             break
